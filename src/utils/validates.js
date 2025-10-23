72669e9c2a94d68f9672e47ec7ad72940922ec83
dannysir <dannysir@naver.com>
2025-10-23 14:37:10 +0900

    feat : 이름 입력 예외 처리
    입력 받은 이름은 반드시 쉼표를 기준으로 구분.
    입력 받은 이름의 크기를 1이상 5 이하.
    중복 이름 불가.

--- src/utils/validates.js
+++ src/utils/validates.js
@@ -1,7 +1,30 @@
 import { ERROR } from '../constants.js';
 
+const validateEachName = (name, duplicate) => {
+  if (name.trim() !== name) {
+    return false;
+  }
+
+  if (name.length <= 0 || name.length > 5) {
+    return false;
+  }
+
+  return !duplicate.has(name);
+};
+
 export const validateNameInput = (input) => {
   let flag = true;
+
+  const nameArray = input.split(',');
+  const duplicate = new Set();
+
+  for (const name of nameArray) {
+    if (!validateEachName(name, duplicate)) {
+      flag = false;
+      break;
+    }
+    duplicate.add(name);
+  }
 
   if (!flag) {
     throw new Error(ERROR.WRONG_NAME_FORMAT);
