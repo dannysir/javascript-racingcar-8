f7a8c0012a818cb8fa45dfc5c12ee21743704b00
dannysir <dannysir@naver.com>
2025-10-23 15:21:03 +0900

    fix : 유닛 테스트 오타 수정
    validateNumberInput 함수 관련 유닛 테스트에서 잘못된 함수를 호출하고 있는 오타 수정.
    통합 테스트에서 잘못된 each 로직 수정.

--- __tests__/unitTest.js
+++ __tests__/unitTest.js
@@ -1,5 +1,5 @@
 import { ERROR } from '../src/constants.js';
-import { validateNameInput } from '../src/utils/validates.js';
+import { validateNameInput, validateNumberInput } from '../src/utils/validates.js';
 
 describe('유효성', () => {
   describe('validateNameInput - 정상 입력', () => {
@@ -23,14 +23,14 @@ describe('유효성', () => {
 
   describe('validateNumberInput - 정상 입력', () => {
     test.each(['1', '0', '111'])('validateNumberInput - 정상 : %s', (input) => {
-      const result = validateNameInput(input);
+      const result = validateNumberInput(input);
       expect(result).toBe(true);
     });
   });
 
   describe('validateNumberInput - 비정상 입력', () => {
     test.each(['-1', 'dannysir', '', '     '])('validateNumberInput - 비정상 : %s', (input) => {
-      expect(() => validateNameInput(input)).toThrow(ERROR.WRONG_NUM_FORMAT);
+      expect(() => validateNumberInput(input)).toThrow(ERROR.WRONG_NUM_FORMAT);
     });
   });
 });
