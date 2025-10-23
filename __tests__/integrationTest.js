d5639cff2f5dbf28ec44cd617c92f777af69dccb
dannysir <dannysir@naver.com>
2025-10-23 15:29:16 +0900

    fix : 통합 테스트 MissionUtils import문 누락 수정.
    MissionUtils import문 누락으로 인한 에러 수정

--- __tests__/integrationTest.js
+++ __tests__/integrationTest.js
@@ -1,6 +1,7 @@
 import App from '../src/App.js';
 import { getLogSpy, mockQuestions, mockRandoms } from '../__mocks__/mocks.js';
 import { INPUT_QUESTION_1, INPUT_QUESTION_2 } from '../src/constants.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 describe('통합 테스트', () => {
   test('기능 테스트', async () => {
